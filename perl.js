2a684b5cec96c373f4b29289cfb8ad44cf49ebaa
worldchanger <lepiromatayo@gmail.com>
2024-10-17 11:42:12 +0300

    innitial commit

---
+++ perl.js
@@ -0,0 +1,7 @@
+const colorButton=document.getElementById('colorButton');
+
+colorButton.addEventListener('click',()=>{
+    colorButton.style.backgroundColor=colorButton.style.backgroundColor === 'blue'? '#4CAF50':'blue';
+
+    }
+);
